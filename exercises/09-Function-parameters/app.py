59d8f417b1a7cdc46fafeb6bc67ebd86609cb520
Lyss2120 <Lys.ivonne@gmail.com>
2023-04-08 04:43:00 +0000

    listo

--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,5 +1,8 @@
 # Your code goes here:
-def render_person(param):
+def render_person(name, age, gender, birthdate, eye_color, ):
+    # param = name + str(birthdate) + fav_color + str(age) + gender
+    name, age, gender, birthdate, eye_color = name, str(birthdate), eye_color, str(age), gender 
+    param = "{} is a {} years old {} born in {} with {} eyes".format(name, str(age), gender, str(birthdate), eye_color)
     return param
 
 
--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,4 +1,10 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
+def sort_names(list):
+    list2 = sorted(list)
+    return list2
 
 print(sort_names(names))
+
+# The sort() method sorts the list ascending by default.
+# You can also make a function to decide the sorting criteria(s).
